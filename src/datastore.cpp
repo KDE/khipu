583396920342ef46b98a04e22f8e689150c78413
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2012-07-22 08:03:13 -0500

    I've forgot save some changes in DataStore, checking ...

--- src/datastore.cpp
+++ src/datastore.cpp
@@ -32,14 +32,6 @@ DataStore::DataStore(QObject* parent)
 {
     m_spacesModel = new SpacesModel(this);
 
-//     m_spacesModel->addSpace(2, "adasd", "333", KIcon("kde").pixmap(QSize(256,256)));
-//     m_spacesModel->addSpace(3, "123 2 234 424 23424 adaadad ad a ada adad sadassd", "333", KIcon("list-add").pixmap(QSize(256,256)));
-//     m_spacesModel->addSpace(2, "adasd", "333", KIcon("roll").pixmap(QSize(256,256)));
-//     m_spacesModel->addSpace(3, "adasd", "333", KIcon("list-remove").pixmap(QSize(256,256)));
-//     m_spacesModel->addSpace(3, "adasd", "333", KIcon("oxygen").pixmap(QSize(256,256)));
-//     m_spacesModel->addSpace(3, "adasd", "333", KIcon("okular").pixmap(QSize(256,256)));
-//     m_spacesModel->addSpace(2, "adasd", "333", KIcon("dolphin").pixmap(QSize(256,256)));
-
     m_variables = new Analitza::Variables;
     
     m_plotsModel = new PlotsModel(this, m_variables);
@@ -66,20 +58,6 @@ DataStore::DataStore(QObject* parent)
 //         this, SLOT(addFuncs(QModelIndex,int,int)));
 //     connect(m_model, SIGNAL(rowsRemoved(QModelIndex,int,int)),
 //         this, SLOT(removeFuncs(QModelIndex,int,int)));
-        
-//     //test code
-//     PlotsModel *model = m_plotsModel;
-// 
-//     model->addPlaneCurve(Analitza::Expression("x->x*x"), "para", Qt::cyan);
-//     model->addPlaneCurve(Analitza::Expression("q->q+2"), "polar simple", Qt::green);
-//     model->addPlaneCurve(Analitza::Expression("t->vector{t*t+1, t+2}"), "vec", Qt::yellow);
-//     model->addPlaneCurve(Analitza::Expression("5*(x**2+y**2)**3=15*(x*y*72)**2"), "impl", Qt::red);
-//     model->addPlaneCurve(Analitza::Expression("x->2+x*x"), "otra simple", Qt::blue);
-//     model->addPlaneCurve(Analitza::Expression("(x**2+y**2)**3=4*(x**2)*(y**2)"), "otra simple", Qt::lightGray);
-//     model->addPlaneCurve(Analitza::Expression("(y-x**2)**2=x*y**3"), "otra simple", Qt::lightGray);
-//     model->addPlaneCurve(Analitza::Expression("sin(x)*sin(y)=1/2"), "otra simple", Qt::yellow);    
-//     model->addPlaneCurve(Analitza::Expression("x->x*x+2"), "asdads", Qt::yellow);
-    
 
 }
 
